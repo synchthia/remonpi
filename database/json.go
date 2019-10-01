5c0a8cb8001a5481bb5ea9827bcaa1aace0fd209
Laica-Lunasys <laica.lunasys@gmail.com>
2019-10-01 11:42:40 +0900

    Add directory check (WIP)

--- database/json.go
+++ database/json.go
@@ -1,12 +1,29 @@
 package database
 
-type Database struct {
-}
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+const FILE_NAME string = "remonpi.json"
 
 func NewDatabase(path string) {
 
 	// Check is exists
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		logrus.WithError(err).Fatal()
+		return
+	}
+
+	// Check file is exists
+	if _, err := os.Stat(path + "/" + FILE_NAME); os.IsNotExist(err) {
+
+	}
 
 	// Create files
+}
+
+func (j *JSONDB) createRemote() {
 
 }
--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ cd $(dirname "$0")
 export GIN_MODE=release
 export REMONPI_VENDOR="mitsubishi"
 export REMONPI_MODEL="kgsa3-c"
-export REMONPI_DB_PATH="$PATH/remonpi.json"
+export REMONPI_DB_PATH="$PWD"
 go run ./cmd/remonpi/main.go
