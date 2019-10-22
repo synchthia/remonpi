4bb7e52058eea5286deb08af26a2694a15a5ac21
Laica Lunasys <laica.lunasys@gmail.com>
2019-10-22 23:21:42 +0900

    Fix sender

--- sender/hexpi.go
+++ sender/hexpi.go
@@ -1,8 +1,10 @@
 package sender
 
 import (
+	"errors"
 	"os"
 	"os/exec"
+	"strconv"
 
 	"github.com/sirupsen/logrus"
 )
@@ -12,19 +14,23 @@ func Send(code []int) error {
 	path := os.Getenv("HEXPI_PATH")
 	if len(path) == 0 {
 		logrus.Fatalf("[Sender] Failed get HEXPI_PATH. defined?")
-		return
+		return errors.New("failed get hexpi_path")
 	}
 
 	gpio := os.Getenv("HEXPI_GPIO")
 	if len(gpio) == 0 {
-		gpio = 2
+		gpio = "2"
 	}
 
-	spacedCode := strings.Concat(code, " ")
+	spacedCode := ""
+	for v := range code {
+		spacedCode += strconv.Itoa(v) + " "
+	}
 
-	res, err := exec.Command(path, gpio, spacedCode).Output()
+	_, err := exec.Command(path, gpio, spacedCode).Output()
 	if err != nil {
 		logrus.WithError(err).Errorf("[Sender] Failed send to hexpi.")
 		return err
 	}
+	return nil
 }
